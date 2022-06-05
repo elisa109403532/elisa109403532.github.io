ad1022b5fe65f30ea5d8a676d923f98af7f0dde0
elisa109403532 <106874612+elisa109403532@users.noreply.github.com>
2022-06-05 20:30:47 +0800

    Update p.php

--- p.php
+++ p.php
@@ -1,7 +1,8 @@
 <?php
             $f = $_GET['f'];
             echo "document.write('".$f."');";
-            data_default_timezone_set('Asia/Taipei');
+?>
+<?php            data_default_timezone_set('Asia/Taipei');
             $datetime = date("H");
 
             if($datetime<"10"){
