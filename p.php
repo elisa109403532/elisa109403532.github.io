67423a0e39456ddd11fe4d34a4af0f783f3bf70d
elisa109403532 <106874612+elisa109403532@users.noreply.github.com>
2022-06-05 20:33:17 +0800

    Update p.php

--- p.php
+++ p.php
@@ -2,16 +2,3 @@
             $f = $_GET['f'];
             echo "document.write('".$f."');";
 ?>
-<?php            data_default_timezone_set('Asia/Taipei');
-            $datetime = date("H");
-
-            if($datetime<"10"){
-                echo "Good morning!";
-            } else if($datetime <"14"){
-                echo "Good afternoon!";
-            } else if($datetime <"18"){
-                echo "Good evening!";
-            } else {
-                echo "Good night!";
-            }
-?>
